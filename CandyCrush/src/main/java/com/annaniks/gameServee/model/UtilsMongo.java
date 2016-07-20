dc79e75251e9b070b90c4054f566981818f6c7e1
artavzd <artavazd256@gmail.com>
2016-07-20 23:33:19 +0400

    The UtilsMongo.java file has been added

---
+++ CandyCrush/src/main/java/com/annaniks/gameServee/model/UtilsMongo.java
@@ -0,0 +1,21 @@
+package com.annaniks.gameServee.model;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+/**
+ * Created by artavzd on 7/20/16.
+ */
+public class UtilsMongo {
+
+    public static boolean isUserExists(String uid, MongoCollection<Document> collection ) {
+        BasicDBObject query = new BasicDBObject();
+        query.put("uid", uid);
+        Document first = collection.find(query).first();
+        if (null != first) {
+            return true;
+        }
+        return false;
+    }
+}
